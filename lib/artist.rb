0e6b2a80006994513b69f1ef833a4bde1e82c9ee
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-10 03:53:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
 
   def self.find_or_create_by_name(name)
     if self.all.find {|artist| artist.name == name}
-      name
+      self.all.find {|artist| artist.name == name}
     else
       self.new(name)
     end
