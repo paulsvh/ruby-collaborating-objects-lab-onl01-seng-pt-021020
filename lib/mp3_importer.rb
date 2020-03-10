9e278e3f6c9b7c76010790be0af3247b74cfa3ed
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-10 03:35:42 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,4 +1,17 @@
 class MP3Importer
+  attr_reader :filepath
+
+  def initialize(filepath)
+    @filepath = filepath
+  end
+
+  def files
+
+  end
+
+  def import(files)
+    files.each{|filename| Song.new_by_filename(filename)}
+  end
 
 
 
