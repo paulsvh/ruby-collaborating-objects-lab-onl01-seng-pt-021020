6affd7f83b64ec8bef02537bf390bed2d6cb6628
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-10 03:00:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,34 @@
 class Artist
 
+  attr_accessor :name
+  @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def add_song(title)
+    title.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def find_or_create_by_name(name)
+    if (self.name.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+
+  def print_songs
+    songs.each {|songs| puts song.name}
+  end
 end
