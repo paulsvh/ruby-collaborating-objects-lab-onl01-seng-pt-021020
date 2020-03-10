690b339b4c086d01fd939b0d7ad74fa05833f5c7
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-10 03:08:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,22 @@ class Song
   def initialize(name)
     @name = name
     @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 
 
 end
